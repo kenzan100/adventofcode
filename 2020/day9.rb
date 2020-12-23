f2e62ea7ff51fb3d2b34e62aca061736ee6d9634
Yuta Miyama <s04155yo@gmail.com>
2020-12-23 11:19:13 -0500

    Day9 part2

--- 2020/day9.rb
+++ 2020/day9.rb
@@ -1,25 +1,43 @@
 require_relative '../helpers/loader.rb'
 
-input = Loader.load(day: 9)
+input = Loader.load(day: 9) #, override: 'day9testinput.txt')
 nums = input.split("\n").map(&:to_i)
 
-window = nums[0..24].dup
-idx = 25
+def part1(nums)
+  window = nums[0..24].dup
+  idx = 25
 
-while idx < nums.size
-  pp idx
-  num = nums[idx]
-  valid = window.combination(2).find { |pair| pair.reduce(&:+) == num }
+  while idx < nums.size
+    num = nums[idx]
+    valid = window.combination(2).find { |pair| pair.reduce(&:+) == num }
 
-  unless valid
-    pp window
-    puts num
-    break
+    unless valid
+      puts num
+      return num
+    end
+    window.shift
+    window.push(num)
+    idx += 1
   end
-  window.shift
-  window.push(num)
-  idx += 1
 end
 
-def combination(n)
+def part2(nums, target)
+  head = 0
+  tail = 1
+
+  while head < nums.size
+    while nums[head..tail].reduce(&:+) <= target
+      return [head, tail] if nums[head..tail].reduce(&:+) == target
+      tail += 1
+    end
+
+    while nums[head..tail].reduce(&:+) >= target
+      return [head, tail] if nums[head..tail].reduce(&:+) == target
+      head += 1
+    end
+  end
 end
+
+head, tail = part2(nums, part1(nums))
+puts nums[head..tail].reduce(&:+)
+puts nums[head..tail].max + nums[head..tail].min
